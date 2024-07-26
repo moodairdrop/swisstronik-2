97efcd63c67738bff333240681de0400b35cc0cb
moodairdrop <advertway@gmail.com>
2024-07-26 15:14:41 +0000

    Initial commit

---
+++ scripts/transfer.js
@@ -0,0 +1,32 @@
+const hre = require("hardhat");
+const { encryptDataField, decryptNodeResponse } = require("@swisstronik/utils");
+const sendShieldedTransaction = async (signer, destination, data, value) => {
+  const rpcLink = hre.network.config.url;
+  const [encryptedData] = await encryptDataField(rpcLink, data);
+  return await signer.sendTransaction({
+    from: signer.address,
+    to: destination,
+    data: encryptedData,
+    value,
+  });
+};
+
+async function main() {
+  const replace_contractAddress = "0x87c5468e6aa5B3DA5CeB29e6B30d562795c86dD8";
+  const [signer] = await hre.ethers.getSigners();
+
+  const replace_contractFactory = await hre.ethers.getContractFactory("TestToken");
+  const contract = replace_contractFactory.attach(replace_contractAddress);
+
+  const replace_functionName = "transfer";
+  const replace_functionArgs = ["0x16af037878a6cAce2Ea29d39A3757aC2F6F7aac1", "1"];
+  const transaction = await sendShieldedTransaction(signer, replace_contractAddress, contract.interface.encodeFunctionData(replace_functionName, replace_functionArgs), 0);
+
+  await transaction.wait();
+  console.log("Transaction Response: https://explorer-evm.testnet.swisstronik.com/tx/" + transaction.hash);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
